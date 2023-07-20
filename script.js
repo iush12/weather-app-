7e4e69bbd0c7e96808c1090c100b507969e35107
iush12 <107797294+iush12@users.noreply.github.com>
2023-07-20 22:10:24 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    const d = new Date();
+    const temperatureElement = document.querySelector('.temp');
+    const descriptionElement = document.querySelector('.txt');
+    
+   
+
+    // Update date
+    document.getElementById("date").innerHTML = d.toDateString();
+};
+
